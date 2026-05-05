c8ad2068fdb09a79c752e29521a3182b46f19320
MaxKong <maxkong@Max-Mac.local>
2026-05-04 20:45:37 -0700

    add problem

---
+++ leetcode/problem 13. Roman to Integer.cpp
@@ -0,0 +1,123 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0;
+        int len;
+        len=s.size();
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]=='I')
+            {
+                if(s[i+1]=='V')
+                {
+                    ans+=4;
+                    i++;
+                }
+                else if(s[i+1]=='X')
+                {
+                    i++;
+                    ans+=9;
+                }
+                else
+                {
+                    ans++;
+                }
+            }
+            if(s[i]=='V')
+                {
+                    ans+=5;
+                    if(i!=0)
+                    {
+                        if(s[i-1]=='I')
+                        {
+                            ans-=5;
+                        }
+                    }
+                }
+            if(s[i]=='X')
+            {
+                if(s[i+1]=='L')
+                {
+                    ans+=40;
+                    i++;
+                }
+                else if(s[i+1]=='C')
+                {
+                    i++;
+                    ans+=90;
+                }
+                else
+                {
+                    ans+=10;
+                    if(i!=0)
+                    {
+                        if(s[i-1]=='I')
+                        {
+                            ans-=10;
+                        }
+                    }
+                }
+            }
+                if(s[i]=='L')
+                {
+                    ans+=50;
+                    if(i!=0)
+                    {
+                        if(s[i-1]=='X')
+                        {
+                            ans-=50;
+                        }
+                    }
+                }
+            if(s[i]=='C')
+            {
+                if(s[i+1]=='D')
+                {
+                    ans+=400;
+                    i++;
+                }
+                else if(s[i+1]=='M')
+                {
+                    i++;
+                    ans+=900;
+                }
+                else
+                {
+                    ans+=100;
+                    if(i!=0)
+                    {
+                        if(s[i-1]=='X')
+                        {
+                            ans-=100;
+                        }
+                    }
+                }
+            }
+            if(s[i]=='D')
+            {
+                    ans+=500;
+                    if(i!=0)
+                    {
+                        if(s[i-1]=='C')
+                        {
+                            ans-=500;
+                        }
+                    }
+            }
+            if(s[i]=='M')
+            {
+                ans+=1000;
+                if(i!=0)
+                {
+                    if(s[i-1]=='C')
+                    {
+                        ans-=1000;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
